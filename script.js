9abffd85622fc2523c6cbe67563e1155535623fa
Rishabh Bhaskar <rishabhaskar.2304@gmail.com>
2020-09-02 17:50:06 +0530

    added input by prompt to reach nodes

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let s = "";
 let nl = "\r\n";
+let pathr = ") > div > div > div > div.copyable-text > div:nth-last-child(1) > span.selectable-text.invisible-space.copyable-text > span";
+let inp = prompt("Paste the JS path :");
+let x = inp.indexOf("(");
+let y = inp.indexOf(")");
+let init = inp.slice(x+1,y);
+let pathl = inp.slice(0,x+1);
+let int = parseInt(init);
 // l = no. of elements you want to fetch
 // m will be the no. of intial element
-let l=7;
+let l=prompt("How many messages do you want to copy?");
  for(i=0;i<l;i++) {
-    let m = 9+i;
+    let m = int+i;
     let n=m.toString();
-    let o = "#main > div._3h-WS > div > div > div.z_tTQ > div:nth-child(" + n + ") > div > div > div > div.copyable-text > div.eRacY > span._3Whw5.selectable-text.invisible-space.copyable-text > span"
-    let k = document.querySelector(o).textContent;
+    let path = pathl + n + pathr;
+    let k = document.querySelector(path).textContent;
     s+=k;
     s+=nl;
   }
-  document.querySelector("#main > footer > div._3ee1T._1LkpH.copyable-area > div._3uMse > div > div._3FRCZ.copyable-text.selectable-text").innerHTML = s;
+  document.querySelector("#main > footer >div:nth-child(1) > div:nth-child(2) > div > div:nth-child(2)").innerHTML = s;
